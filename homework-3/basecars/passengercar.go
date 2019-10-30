a2a63951ae81261d9f5cd70af2819986ea10f24f
mvgrachev <mvgrachev@MacBook-Pro.local>
2019-10-30 23:39:46 +0300

    Добавляем несколько структур и работаем с их свойствами

---
+++ homework-3/basecars/passengercar.go
@@ -0,0 +1,7 @@
+package basecars
+
+type PassengerCar struct {
+	BaseCar
+	PassCapacity float64
+	PassFill float64
+}
---
+++ homework-3/basecars/truckcar.go
@@ -0,0 +1,7 @@
+package basecars
+
+type TruckCar struct {
+	BaseCar
+	TruckCapacity float64
+	TruckFill float64
+}
