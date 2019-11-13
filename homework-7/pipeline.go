3f66f52ae10b4e10fc982e91fa4f93df7323e94e
mvgrachev <mvgrachev@MacBook-Pro.local>
2019-11-14 01:13:40 +0300

    Выполняем домашнее задание #7

---
+++ homework-7/pipeline.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	// генерация
+	go func(naturals chan int) {
+		for x := 0; x < 10; x++ {
+			naturals <- x
+		}
+		close(naturals)
+	}(naturals)
+
+	// возведение в квадрат
+	go func(naturals chan int) {
+		for {
+			x, ok := <-naturals
+			
+			if !ok {
+				break
+			}
+			squares <- x * x
+		}
+		close(squares)
+	}(naturals)
+
+	// печать
+	for {
+		x, ok := <-squares
+		if !ok {
+			break
+		}
+		fmt.Println(x)
+	}
+}
